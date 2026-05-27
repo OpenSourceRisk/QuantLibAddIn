650d240ffe3f031661419e6ae5b88804c57e02e4
Eric Ehlers <eric.ehlers@lseg.com>
2026-05-27 15:29:23 +0200

    ACADIAQPR-14118 Migrate QuantLibAddin to the ORE fork of QuantLib - remove log4cxx

--- ObjectHandler/oh/logger.hpp
+++ ObjectHandler/oh/logger.hpp
@@ -25,20 +25,8 @@
 #define oh_logger_hpp
 
 #include <oh/singleton.hpp>
-
-// Some older builds of log4cxx did not include file log4cxxdefines.h.
-// If the #include below fails, it means either that you have the wrong
-// version of log4cxx, or that your log4cxx installation (if any) cannot
-// be found at all.
-#include <log4cxx/log4cxxdefines.h>
-#if LOG4CXX_VERSION != 0x001000f15
-    #error using an incorrect version of log4cxx, please update.
-#endif
-
-#include <log4cxx/logger.h>
-#include <log4cxx/simplelayout.h>
-#include <log4cxx/fileappender.h>
-#include <log4cxx/consoleappender.h>
+#include <fstream>
+#include <string>
 
 namespace ObjectHandler {
 
@@ -109,17 +97,13 @@ namespace ObjectHandler {
         //@}
 
 
-        virtual ~Logger(){}
+        virtual ~Logger();
     private:
         Logger();
-        //log4cxx::LoggerPtr _logger;
-        //log4cxx::LayoutPtr _layout;
-        //log4cxx::AppenderPtr _fileAppender;
-        //log4cxx::AppenderPtr _consoleAppender;
-        log4cxx::LayoutPtr getLayout();
-
-        //log4cxx::AppenderPtr fileAppender_;
+        std::ofstream file_;
         std::string filename_;
+        int level_;
+        bool console_;
     };
 
 }
--- ObjectHandler/oh/utilities.cpp
+++ ObjectHandler/oh/utilities.cpp
@@ -24,9 +24,7 @@
 #include <oh/config.hpp>
 #endif
 #include <oh/utilities.hpp>
-#ifdef OH_INCLUDE_LOG4CXX
 #include <oh/logger.hpp>
-#endif
 #include <oh/repository.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
@@ -55,70 +53,42 @@ namespace ObjectHandler {
 
     std::string logSetFile(const std::string &logFileName,
                            const int &logLevel) {
-#ifdef OH_INCLUDE_LOG4CXX
         Logger::instance().setFile(logFileName, logLevel);
         return logFileName;
-#else
-        return std::string();
-#endif
     }
 
     DLL_API void logWriteMessage(const std::string &message,
                                  const int &level) {
-#ifdef OH_INCLUDE_LOG4CXX
         Logger::instance().writeMessage(message, level);
-#else
-        std::cout << "LOG - " << message << std::endl;
-#endif
     }
 
     void logSetLevel(const int &logLevel) {
-#ifdef OH_INCLUDE_LOG4CXX
         Logger::instance().setLevel(logLevel);
-#endif
     }
 
     const std::string logFile(){
-#ifdef OH_INCLUDE_LOG4CXX
         return Logger::instance().file();
-#else
-        return std::string();
-#endif
     }
 
     const int logLevel(){
-#ifdef OH_INCLUDE_LOG4CXX
         return Logger::instance().level();
-#else
-        return 0;
-#endif
     }
 
     void logSetConsole(const int &console,
                       const int &logLevel) {
-#ifdef OH_INCLUDE_LOG4CXX
-            Logger::instance().setConsole(console, logLevel);
-#endif
+        Logger::instance().setConsole(console, logLevel);
     }
 
     void logObject(const std::string &objectID) {
         std::ostringstream msg;
         Repository::instance().dumpObject(objectID, msg);
-#ifdef OH_INCLUDE_LOG4CXX
         Logger::instance().writeMessage(msg.str());
-#else
-        std::cout << "LOG - " << msg.str() << std::endl;
-#endif
     }
 
     void logAllObjects() {
         std::ostringstream msg;
         Repository::instance().dump(msg);
-#ifdef OH_INCLUDE_LOG4CXX
         Logger::instance().writeMessage(msg.str());
-#else
-        std::cout << "LOG - " << msg.str() << std::endl;
-#endif
     }
 
     std::vector<std::string> split(const std::string& line,
